4e5ca9754e18ef22eb8e6655c85e1aef2b36cd87
AkiraFuku <k024g1002@g.neec.ac.jp>
2026-06-10 15:32:39 +0900

    輝度ベース

--- project/OffScreen.cpp
+++ project/OffScreen.cpp
@@ -77,7 +77,61 @@ void OffScreen::Initialize()
     psoConfig.shaderPaths.clear();  psoConfig.shaderPaths = {
         { ShaderType::VS, L"resources/shaders/CopyImage/FullScreen.vs.hlsl", "main", L"vs_6_0" },
         //{ ShaderType::PS, L"resources/shaders/Smooth/BoxFilter.ps.hlsl", "main", L"ps_6_0" }
-        { ShaderType::PS, L"resources/shaders/GaussianFilter/GaussianFilter.ps.hlsl", "main", L"ps_6_0" }
+        { ShaderType::PS, L"resources/shaders/OutLine/LuminanceBasedOutline.ps.hlsl", "main", L"ps_6_0" }
+    };
+    
+    psoConfig.rootSignatureGenerator = []() {
+        // ラムダ式内での初期化
+        HRESULT hr; 
+        std::vector<D3D12_STATIC_SAMPLER_DESC> staticSamplers;
+        D3D12_STATIC_SAMPLER_DESC sampler = PSOManager::GetInstance()->StaticSamplers();
+        staticSamplers.push_back(sampler);
+
+        D3D12_DESCRIPTOR_RANGE descRangeTexture[1]{};
+        descRangeTexture[0].BaseShaderRegister = 0;
+        descRangeTexture[0].NumDescriptors = 1;
+        descRangeTexture[0].RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
+        descRangeTexture[0].OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
+
+        CD3DX12_ROOT_PARAMETER rootParameters[1]{};
+        rootParameters[0].InitAsDescriptorTable(1, &descRangeTexture[0]);
+
+        CD3DX12_ROOT_SIGNATURE_DESC rootSignatureDesc{};
+        rootSignatureDesc.Init(_countof(rootParameters), rootParameters, (UINT)staticSamplers.size(), staticSamplers.data(), D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT);
+
+        Microsoft::WRL::ComPtr<ID3DBlob> signatureBlob;
+        Microsoft::WRL::ComPtr<ID3DBlob> errorBlob;
+
+        hr = D3D12SerializeRootSignature(&rootSignatureDesc, D3D_ROOT_SIGNATURE_VERSION_1, &signatureBlob, &errorBlob);
+        if (FAILED(hr)) {
+            if (errorBlob) Logger::Log(reinterpret_cast<char*>(errorBlob->GetBufferPointer()));
+            assert(false);
+        }
+
+        Microsoft::WRL::ComPtr<ID3D12RootSignature> rootSignature;
+        // hr = ... (ここでは auto を付けない)
+        hr = DXCommon::GetInstance()->GetDevice()->CreateRootSignature(0, signatureBlob->GetBufferPointer(), signatureBlob->GetBufferSize(), IID_PPV_ARGS(&rootSignature));
+        assert(SUCCEEDED(hr));
+        return rootSignature;
+    };
+
+    psoConfig.inputLayoutGenerator = []() {
+
+         
+        InputLayout inputLayout={};
+        inputLayout.inputLayout= D3D12_INPUT_LAYOUT_DESC{ nullptr, 0 };;
+        return inputLayout;
+
+    };
+    psoConfig.depthEnable = false;
+
+    PSOManager::GetInstance()->RegisterPsoGenerator("Vignette", psoConfig);
+    psoConfig = {};
+
+    psoConfig.shaderPaths.clear();  psoConfig.shaderPaths = {
+        { ShaderType::VS, L"resources/shaders/CopyImage/FullScreen.vs.hlsl", "main", L"vs_6_0" },
+        //{ ShaderType::PS, L"resources/shaders/Smooth/BoxFilter.ps.hlsl", "main", L"ps_6_0" }
+        { ShaderType::PS, L"resources/shaders/OutLine/DepthBasedOutline.ps.hlsl", "main", L"ps_6_0" }
     };
     
     psoConfig.rootSignatureGenerator = []() {
